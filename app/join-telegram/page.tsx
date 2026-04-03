fb0c5f9e4a30089b054f7f353ac029844ece7577
rakeshkhans <rakesh.khan7786@gmail.com>
2026-04-03 20:38:16 +0530

    Update page.tsx

--- app/join-telegram/page.tsx
+++ app/join-telegram/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-const TELEGRAM_LINK = "https://t.me/AllYonoGamesh"; // Apna Telegram link yahan daalo
+const TELEGRAM_LINK = "https://t.me/allyonogameshcom"; // Apna Telegram link yahan daalo
 
 export default function JoinTelegram() {
   redirect(TELEGRAM_LINK);
